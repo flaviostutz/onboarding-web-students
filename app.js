7d2574d8884e8d7cbdd354c0f6f8ca044f9f22a6
MoisesFGNeto <117291517+MoisesFGNeto@users.noreply.github.com>
2024-02-08 21:58:59 +0100

    Form from Figma created

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+//page- Hidde2.html
 function redirectTo(){
   window.location.href = "hidden2.html";
 }
-  
+
+//page- hidden3.html
+function alertSubmit() {
+  alert("Almost there, but we have no server yet!")
+}
