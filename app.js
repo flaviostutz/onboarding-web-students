a379a160ec5586aca0b9986f6507033791e66b36
MoisesFGNeto <117291517+MoisesFGNeto@users.noreply.github.com>
2024-02-08 10:05:16 +0100

    commented code removed

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-// let zoomed = false;
-
-// function toggleZoom() {
-//   const image = document.getElementById('hiddenImage');
-//   zoomed = !zoomed; 
-//   if (zoomed) {
-//     image.classList.add('zoomed');
-//     image.classList.remove('hidden-page');
-//     image.style.cursor = 'zoom-out';
-//   } else {
-//     image.classList.add('hidden-page');
-//     image.classList.remove('zoomed');
-//     image.style.cursor = 'zoom-in';
-//   }
-// }
-
 function redirectTo(){
   window.location.href = "hidden2.html";
 }
