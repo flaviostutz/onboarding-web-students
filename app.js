ef4bc134f7034bcda40d42419c0fe228057a4a9c
MoisesFGNeto <117291517+MoisesFGNeto@users.noreply.github.com>
2024-02-08 01:10:43 +0100

    Page /hidden2.html created, when the user clicks on the img of /hidden.html redirect to this new page (hidden2.html)

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-let zoomed = false;
+// let zoomed = false;
 
-function toggleZoom() {
-  const image = document.getElementById('hiddenImage');
-  zoomed = !zoomed; 
-  if (zoomed) {
-    image.classList.add('zoomed');
-    image.classList.remove('hidden-page');
-    image.style.cursor = 'zoom-out';
-  } else {
-    image.classList.add('hidden-page');
-    image.classList.remove('zoomed');
-    image.style.cursor = 'zoom-in';
-  }
+// function toggleZoom() {
+//   const image = document.getElementById('hiddenImage');
+//   zoomed = !zoomed; 
+//   if (zoomed) {
+//     image.classList.add('zoomed');
+//     image.classList.remove('hidden-page');
+//     image.style.cursor = 'zoom-out';
+//   } else {
+//     image.classList.add('hidden-page');
+//     image.classList.remove('zoomed');
+//     image.style.cursor = 'zoom-in';
+//   }
+// }
+
+function redirectTo(){
+  window.location.href = "hidden2.html";
 }
+  
